dd640d35a212284d0010523c97fc976b85bbcb22
KevOneRedOne <kevinalves@hotmail.fr>
2024-02-01 17:24:06 +0100

    feat(frontend): add backoffice pages

---
+++ src/app/backoffice/home/page.js
@@ -0,0 +1,17 @@
+import TitlePage from "@/components/UI/TitlePage";
+
+const BackOfficeHome = () => {
+    return (
+        <div className="container mx-auto">
+            <TitlePage title="MyStore - BackOffice" />
+            <div className="min-h-screen">
+                <h2 className="text-2xl mb-4">BackOffice</h2>
+                <div className="mb-8">
+                   
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BackOfficeHome;
---
+++ src/app/backoffice/page.js
@@ -0,0 +1,17 @@
+import TitlePage from "@/components/UI/TitlePage";
+
+const BackOffice = () => {
+    return (
+        <div className="container mx-auto">
+            <TitlePage title="MyStore - BackOffice" />
+            <div className="min-h-screen">
+                <h2 className="text-2xl mb-4">Login Admin</h2>
+                <div className="mb-8">
+                   
+                </div>
+            </div>
+        </div>
+    );  
+};
+
+export default BackOffice;
