062af579660f75cbe10069ae356b9a8cef08a8c1
KevOneRedOne <kevinalves@hotmail.fr>
2024-02-02 20:56:18 +0100

    Update Login Admin

--- src/app/backoffice/page.js
+++ src/app/backoffice/page.js
@@ -1,17 +1,45 @@
-import TitlePage from "@/components/UI/TitlePage";
+'use client';
 
-const BackOffice = () => {
+import TitlePage from "@/components/UI/TitlePage";
+import Input from '@/components/UI/Input';
+import Button from '@/components/UI/Button';
+
+import { useState } from 'react';
+
+const LoginFormAdmin = () => {
+    const [userForm, setUserForm] = useState ({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        setUserForm({...userForm, [e.target.name]: e.target.value})
+    }
+
+    const submit = async (e) => {
+        e.preventDefault();
+        console.log(userForm.email, userForm.password);
+    }
+
     return (
-        <div className="container mx-auto">
-            <TitlePage title="MyStore - BackOffice" />
-            <div className="min-h-screen">
-                <h2 className="text-2xl mb-4">Login Admin</h2>
-                <div className="mb-8">
-                   
+        <div className="container mx-auto bg-gray-100">
+            <div className="min-h-screen flex items-center justify-center bg-gray-100 align-center">
+                <div className="bg-white p-8 rounded shadow-md w-96 xl:w-1/3">
+                    <h2 className="text-2xl font-bold mb-6">Login Administrateur</h2> 
+                    <form onSubmit={(e)=> submit(e)}>
+                        <Input label={'Adresse email'} name={'email'} value={userForm.email} placeholder={'email'} type={'email'} onChange={(e) => handleChange(e)} isRequired={true} />
+                        <br />
+                        <Input label={'Mot de passe'} name={'password'} value={userForm.password} placeholder={'mot de passe'} type={'password'} onChange={(e) => handleChange(e)} isRequired={true} />
+                        <br />
+                        <Button type={'submit'} title={'Se connecter'}/>
+                    </form>
                 </div>
             </div>
         </div>
     );  
 };
 
-export default BackOffice;
+export default LoginFormAdmin;
+
+
+
--- src/components/UI/TitlePage/index.js
+++ src/components/UI/TitlePage/index.js
@@ -1,7 +1,7 @@
 const Index = ({title}) => {
   return (
     <div className="my-12">
-      <h1 className="text-3xl font-medium font-display text-slate-500">
+      <h1 className="text-3xl font-medium text-slate-500">
         {title}
       </h1>
     </div>
