932bc9d847de0c2c0158af473c8deee931c61340
KGROLLEMUND <kyllian.grollemund@ynov.com>
2024-03-21 11:46:58 +0100

    create page whishlist

---
+++ src/app/whishlist/page.js
@@ -0,0 +1,14 @@
+import TitlePage from "@/components/UI/TitlePage";
+
+const Page = () => {
+    return (
+        <div className="container mx-auto">
+            <TitlePage title="Whishlist" />
+            <div className="min-h-screen">
+                <h2 className="text-2xl mb-4">Whishlist</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Page;
