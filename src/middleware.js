2390e1f3c76ed57509b271a5b594f252ef210c2c
KevOneRedOne <kevinalves@hotmail.fr>
2024-04-19 17:57:35 +0200

    Fix login redirect issue in middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -3,27 +3,27 @@ import { NextRequest } from "next/server";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
-//TODO: change url for production
+//TODO: change url for production or development
 export function middleware(request) {
     const token = request.cookies.get('token');
     
     if (!token) {
-        // return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`);
-        return NextResponse.redirect(`http://localhost:3000/auth/login`);
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`);
+        // return NextResponse.redirect(`http://localhost:3000/auth/login`);
     }
     
     try {
         const decodedToken= jwt.decode(token.value);
 
         if (!decodedToken.admin){
-            // return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user/me`);
-            return NextResponse.redirect(`http://localhost:3000/user/me`);
+            return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user/me`);
+            // return NextResponse.redirect(`http://localhost:3000/user/me`);
         };
          
     } catch (error) {
         console.error("Token verification error:", error);
-        // return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`);
-        return NextResponse.redirect(`http://localhost:3000/auth/login`);
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`);
+        // return NextResponse.redirect(`http://localhost:3000/auth/login`);
     }
 };
 
