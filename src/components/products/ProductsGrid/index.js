892184235c223e0da55911cbbbdcc4ca8b1a733d
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2024-04-19 15:18:09 +0200

    fix active fasle being displayed

--- src/components/products/ProductsGrid/index.js
+++ src/components/products/ProductsGrid/index.js
@@ -1,15 +1,15 @@
 import ProductCard from "@/components/products/ProductCard";
 
-const Index = ({products}) => {
-  return (
-    <div className="grid grid-cols-4 gap-8 my-12">
-      {
-        products.map(product => (
-          <ProductCard key={product._id} product={product} />
-        ))
-      }
-    </div>
-  );
-}
+const Index = ({ products }) => {
+    return (
+        <div className="grid grid-cols-4 gap-8 my-12">
+            {products.map((product) =>
+                product.active ? (
+                    <ProductCard key={product._id} product={product} />
+                ) : null
+            )}
+        </div>
+    );
+};
 
 export default Index;
